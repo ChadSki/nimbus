1cb3454e8675cedaa06d212a4e1a9dc595312299
Chad Zawistowski <chad.zawistowski@gmail.com>
2013-09-03 21:06:01 -0400

    Playing with mem_example

--- mem_example.py
+++ mem_example.py
@@ -2,12 +2,22 @@ from halolib import *
 load_plugins('.\plugins')
 m = load_map_from_memory(fix_video_render=True)
 
-
-ass_rifle = m.get_tag('weap', 'ass')
+# weaps
+rifle = m.get_tag('weap', 'assault rifle')
 pistol = m.get_tag('weap', '\\pistol')
-charged = m.get_tag('proj', 'charged')
+banshee_gun = m.get_tag('weap', 'banshee')
+
+# vehis
+banshee = m.get_tag('vehi', 'banshee')
+
+# projs
 rocket = m.get_tag('proj', 'rocket')
+plasma = m.get_tag('proj', 'plasma grenade')
+plasma.initial_velocity = 0.2
+plasma.final_velocity = 0.2
 
-ass_rifle.triggers[0].projectile = charged
-pistol.triggers[0].projectile = m.get_tag('vehi', 'banshee')
+rifle.triggers[0].projectile = plasma
+pistol.triggers[0].projectile = plasma
 
+#banshee_gun.triggers[0].projectile = rocket
+#banshee_gun.triggers[1].projectile = m.get_tag('vehi', 'scorpion')
