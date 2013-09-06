4a3779602161cc0f9e955eba526508a70b98963d
Chad Zawistowski <chad.zawistowski@gmail.com>
2013-09-05 21:07:02 -0400

    Add version placeholder for now

--- byteaccess/__init__.py
+++ byteaccess/__init__.py
@@ -1,2 +1,4 @@
 from byteaccess.byteaccess import ByteAccess, access_over_file
 from byteaccess.winmemaccess import access_over_process
+
+__version__ = 'TODO: Figure out Python 3 version conventions'
