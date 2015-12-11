20d9c701c6f068da4a5c8683a5700d00a18895a5
Chad Zawistowski <chad.zawistowski@gmail.com>
2015-12-11 01:17:36 -0800

    Better tests

--- byteaccess/test/test_byteaccess.py
+++ byteaccess/test/test_byteaccess.py
@@ -7,48 +7,55 @@
 import unittest
 import byteaccess
 
-def string_tests(context, offset):
-    """Test some string functions of ByteAccess with overlapping access regions"""
-    foo = context.ByteAccess(offset, 21)
-    bar = context.ByteAccess(offset, 4)
+import abc
 
-    assert foo.read_bytes(0, 4) == b'asdf'
-    assert foo.read_bytes(4, 10) == b'0123456789'
-    foo.write_bytes(0, b'test')
-    assert foo.read_bytes(0, 4) == b'test'
-    assert bar.read_all_bytes() == foo.read_bytes(0, 4)
-    bar.write_bytes(0, b'asdf')
-    assert foo.read_bytes(0, 4) == b'asdf'
+class BaseByteAccessTest(object):
 
-def number_tests(context, offset):
-    """Test some number functions of ByteAccess with overlapping access regions"""
-    foo = context.ByteAccess(offset, 21)
-    baz = context.ByteAccess(offset + 15, 16)
+    def test_strings(self):
+        """Test some string functions of ByteAccess with overlapping access regions"""
+        foo = self.context.ByteAccess(self.offset, 21)
+        bar = self.context.ByteAccess(self.offset, 4)
 
-    foo.write_float32(0, 2.0)
-    foo.write_float64(4, 15.3)
-    assert foo.read_float32(0) == 2.0
-    assert foo.read_float64(4) == 15.3
+        foo.write_bytes(0, b'test')
+        assert foo.read_bytes(0, 4) == b'test'
+        assert bar.read_all_bytes() == foo.read_bytes(0, 4)
+        bar.write_bytes(0, b'asdf')
+        assert foo.read_bytes(0, 4) == b'asdf'
 
-    foo.write_int8(0, 12)
-    foo.write_int16(1, 13)
-    foo.write_int32(3, 14)
-    foo.write_int64(7, 14)
-    assert foo.read_int8(0) == 12
-    assert foo.read_int16(1) == 13
-    assert foo.read_int32(3) == 14
-    assert foo.read_int64(7) == 14
+    def test_numbers(self):
+        """Test some number functions of ByteAccess with overlapping access regions"""
+        foo = self.context.ByteAccess(self.offset, 21)
+        baz = self.context.ByteAccess(self.offset + 15, 16)
+
+        foo.write_float32(0, 2.0)
+        foo.write_float64(4, 15.3)
+        assert foo.read_float32(0) == 2.0
+        assert foo.read_float64(4) == 15.3
+
+        foo.write_int8(0, 12)
+        foo.write_int16(1, 13)
+        foo.write_int32(3, 14)
+        foo.write_int64(7, 14)
+        assert foo.read_int8(0) == 12
+        assert foo.read_int16(1) == 13
+        assert foo.read_int32(3) == 14
+        assert foo.read_int64(7) == 14
 
 
-class MyTest(unittest.TestCase):
+class FileByteAccessTest(unittest.TestCase, BaseByteAccessTest):
 
-    def test_filebyteaccess(self):
+    def setUp(self):
         import os
         this_folder = os.path.dirname(os.path.realpath(__file__))
-        context = byteaccess.FileContext(os.path.join(this_folder, 'testfile.bin'))
-        string_tests(context, 0)
-        number_tests(context, 0)
+        self.context = byteaccess.FileContext(os.path.join(this_folder, 'testfile.bin'))
+        self.offset = 0
 
+class WinMemByteAccessTest(unittest.TestCase, BaseByteAccessTest):
+
+    def setUp(self):
+        self.context = byteaccess.MemContext('halo')
+        # in the mapheader, 100 bytes in is a suitable scratch area
+        self.offset = 0x6A8154 + 100
 
     def test_membyteaccess(self):
         context = byteaccess.MemContext('halo')
